cfacb85881d1ec34382f53f5485f39127ef7d6b4
Roman Loginov <66028178+RomaGrek@users.noreply.github.com>
2021-10-02 10:06:17 +0300

    Update Checker.java
    gg

--- Web-4-backend-master/src/main/java/main/web/service/Checker.java
+++ Web-4-backend-master/src/main/java/main/web/service/Checker.java
@@ -4,24 +4,6 @@ import main.web.entity.Point;
 
 public class Checker {
 
-//    public static void hit(Point point) {
-//        double x = point.getX();
-//        double y = point.getY();
-//        double r = point.getR();
-//        if(x>=0 && y>=0){
-//            point.setInArea(y <= -x +r/2);
-//            return;
-//        }
-//        if(x>=0 && y<=0){
-//            point.setInArea( Math.pow(x, 2) + Math.pow(y,2) <= Math.pow(r/2, 2));
-//            return;
-//        }
-//        if(x<0 && y>=0){
-//            point.setInArea( -x<=r/2 && y<=r);
-//            return;
-//        }
-//        point.setInArea(false);
-//    }
 
     /* статический метод для проверки попадания в цель */
     public static void hit(Point point) {
